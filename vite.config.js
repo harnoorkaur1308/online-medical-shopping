a23be4ce2f32ff2c40bdf2fa76438b5614e7243d
harnoorkaur1308 <harnoorkaur1308@gmail.com>
2026-03-31 22:14:26 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Vite configuration - standard React setup
+export default defineConfig({
+  plugins: [react()],
+})
